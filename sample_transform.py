3020514a37d84ad95258f8e086e9f974b3cf9e75
Noah Leigh <noah.d.leigh@gmail.com>
2017-11-16 14:20:11 -0500

    Fixed function names

--- sample_transform.py
+++ sample_transform.py
@@ -5,8 +5,8 @@ _scaling_factor = 2
 
 TRANSFORMS = [
     (effects.desample, {'factor': _scaling_factor}),
-    (effects.split_channels, {'offset': 1}),
-    (effects.wave_distortion, {'mag': 3, 'freq': 1}),
+    (effects.split_color_channels, {'offset': 1}),
+    (effects.sin_wave_distortion, {'mag': 3, 'freq': 1}),
     (effects.walk_distortion, {'max_step_length': 1}),
     (effects.shift_corruption, {'offset_mag': 2, 'ratio': 0.25}),
     (effects.convert, {'mode': 'RGB'}),
