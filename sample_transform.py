f10160438511511ad7373a8f8878d2607bba1701
Noah Leigh <noah.d.leigh@gmail.com>
2017-11-16 23:39:39 -0500

    Added new functions to sample_transform

--- sample_transform.py
+++ sample_transform.py
@@ -4,15 +4,17 @@ import effects
 _scaling_factor = 2
 
 TRANSFORMS = [
+    (effects.convert, {'mode': 'RGB'}),
+    # (effects.desample, {'factor': 4}),
     (effects.desample, {'factor': _scaling_factor}),
     (effects.split_color_channels, {'offset': 1}),
+    (effects.swap_cells, {'rows': 20, 'cols': 20, 'swaps': 4}),
+    (effects.add_noise_bands, {'count': 4, 'thickness': 10}),
     (effects.sin_wave_distortion, {'mag': 3, 'freq': 1}),
     (effects.walk_distortion, {'max_step_length': 1}),
     (effects.shift_corruption, {'offset_mag': 2, 'coverage': 0.25}),
-    (effects.convert, {'mode': 'RGB'}),
     (ImageOps.posterize, {'bits': 3}),
-    # (ImageOps.solarize, {'threshold': 200}),
-    (effects.sharpen, {'factor': 2.0}),
     (effects.upscale, {'factor': _scaling_factor}),
+    (effects.sharpen, {'factor': 2.0}),
     (effects.add_transparent_pixel, {}),
 ]
