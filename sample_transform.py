8582f0b388ba1d5f83132fb10cd7cb89d8439690
Noah Leigh <noah.d.leigh@gmail.com>
2017-11-16 19:18:32 -0500

    Fixed wrong function argument name

--- sample_transform.py
+++ sample_transform.py
@@ -8,7 +8,7 @@ TRANSFORMS = [
     (effects.split_color_channels, {'offset': 1}),
     (effects.sin_wave_distortion, {'mag': 3, 'freq': 1}),
     (effects.walk_distortion, {'max_step_length': 1}),
-    (effects.shift_corruption, {'offset_mag': 2, 'ratio': 0.25}),
+    (effects.shift_corruption, {'offset_mag': 2, 'coverage': 0.25}),
     (effects.convert, {'mode': 'RGB'}),
     (ImageOps.posterize, {'bits': 3}),
     # (ImageOps.solarize, {'threshold': 200}),
