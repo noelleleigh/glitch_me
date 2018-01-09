540cb9b34a1979e1f34e19b587157988880ac59c
Noah Leigh <noah.d.leigh@gmail.com>
2018-01-09 01:28:11 -0500

    Made the GIF transforms a seamless loop, Cleaned up some lines

--- sample_transform.py
+++ sample_transform.py
@@ -7,19 +7,24 @@ STATIC_TRANSFORM: List of 2-tuples:
 
 GIF_TRANSFORM: Function that returns a list in the form of STATIC_TRANSFORM
 """
+from math import pi, cos
 from PIL import Image, ImageOps
 import effects
 
+
 STATIC_TRANSFORM = [
     (effects.convert, {'mode': 'RGB'}),
-    (effects.pixel_sort, {'mask_function': lambda val: 255 if val < 100 else 0}),
+    (effects.pixel_sort, {
+        'mask_function': lambda val: 255 if val < 100 else 0
+    }),
     (effects.split_color_channels, {'offset': 1}),
-    (effects.swap_cells, {'rows': 20, 'cols': 20, 'swaps': 4}),
     (effects.add_noise_bands, {'count': 4, 'thickness': 10}),
     (effects.sin_wave_distortion, {'mag': 3, 'freq': 1}),
     (effects.walk_distortion, {'max_step_length': 1}),
     (effects.shift_corruption, {'offset_mag': 2, 'coverage': 0.25}),
-    (ImageOps.posterize, {'bits': 3}),
+    (effects.low_res_blocks, {
+        'rows': 15, 'cols': 15, 'cells': 4, 'factor': 2
+    }),
     (effects.sharpen, {'factor': 2.0}),
     (effects.add_transparent_pixel, {}),
 ]
@@ -38,15 +43,28 @@ def GIF_TRANSFORM(
     median_lum: The median luminance of the image (integer from 0 to 255),
         useful for controlling pixel sorting.
     """
+    loop_progress = -cos(2*pi*progress)/2 + 0.5
+    lum_limit = median_lum + 0.5 * median_lum
+    lum_limit = lum_limit if lum_limit <= 255 else 255
+
     return [
         (effects.convert, {'mode': 'RGB'}),
         (effects.pixel_sort, {
             'mask_function':
-            lambda val, progress=progress, median_lum=median_lum:
-                255 if val < median_lum * progress else 0
+            lambda val, factor=loop_progress, limit=lum_limit:
+                255 if val < limit * factor else 0,
+            'reverse': True
         }),
-        (effects.sin_wave_distortion, {'mag': 10 * progress, 'freq': 2 * progress}),
-        (effects.add_noise_bands, {'count': int(10 * progress), 'thickness': 10}),
-        (effects.split_color_channels, {'offset': int(5 * progress)}),
+        (effects.sin_wave_distortion, {
+            'mag': 5, 'freq': 1, 'phase': -2*pi*progress
+        }),
+        (effects.add_noise_bands, {
+            'count': int(10 * loop_progress), 'thickness': 10
+        }),
+        (effects.low_res_blocks, {
+            'rows': 10, 'cols': 10, 'cells': int(10 * progress), 'factor': 4
+        }),
+        (ImageOps.posterize, {'bits': int(5 * (1 - loop_progress) + 3)}),
+        (effects.split_color_channels, {'offset': int(5 * loop_progress)}),
         (effects.convert, {'mode': 'P', 'palette': Image.ADAPTIVE}),
     ]
