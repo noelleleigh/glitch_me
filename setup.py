89aeadc664a8fc0f830bb2a05b75a8cf4a3d13cc
Noah Leigh <noah.d.leigh@gmail.com>
2018-01-18 19:44:09 -0500

    Added setup and manifest files

---
+++ setup.py
@@ -0,0 +1,35 @@
+"""Install script for glitch_me."""
+from setuptools import setup
+
+setup(
+    name='glitch_me',
+    version='1.0.0',
+    description='Python module to add some distortion/glitch effects to images',
+    # TODO: Add long_description
+    url='https://github.com/noahleigh/glitch_me',
+    author='Noah Leigh',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Artistic Software',
+        'Topic :: Multimedia :: Graphics :: Graphics Conversion',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Natural Language :: English',
+
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    keywords='glitch images art',
+    py_modules=['glitch_me'],
+    install_requires=[
+        'Pillow>=5'
+    ],
+    python_requires='~=3.6',
+    entry_points={
+        'console_scripts': [
+            'glitch_me=glitch_me.__main__:main',
+        ],
+    },
+)
