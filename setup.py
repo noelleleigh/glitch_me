cbd17643c10108ce199b022c8c0e0b226225e234
Noah Leigh <noah.d.leigh@gmail.com>
2018-08-03 20:00:57 -0400

    Updated readme in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 """Install script for glitch_me."""
 from setuptools import setup
+import os
+readme_file = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    'README.md'
+)
+try:
+    from m2r import parse_from_file
+    readme = parse_from_file(readme_file)
+except ImportError:
+    # m2r may not be installed in user environment
+    with open(readme_file) as f:
+        readme = f.read()
 
 setup(
     name='glitch_me',
-    version='1.0.0',
+    version='1.0.3',
     description='Python module to add some distortion/glitch effects to images',
-    # TODO: Add long_description
+    long_description=readme,
     url='https://github.com/noahleigh/glitch_me',
     author='Noah Leigh',
     classifiers=[
