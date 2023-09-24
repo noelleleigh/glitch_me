e143020ddd865b25b65245028aad0edaffd2edf5
Noelle Leigh <noelle_leigh@fastmail.com>
2023-09-24 12:13:13 -0400

    Make-Readme-Examples.ps1: Use `python`

--- Make-Readme-Examples.ps1
+++ Make-Readme-Examples.ps1
@@ -1,2 +1,2 @@
-py -m glitch_me still ./readme_assets/*_small.png .\readme_assets;
-py -m glitch_me gif ./readme_assets/*_small.png .\readme_assets;
+python -m glitch_me still ./readme_assets/*_small.png .\readme_assets;
+python -m glitch_me gif ./readme_assets/*_small.png .\readme_assets;
